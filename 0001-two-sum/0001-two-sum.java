cfc26beeef9ecb24e2016a5bf170bf66da0fa1f5
JaeWon Choi <codesver@gmail.com>
2023-03-23 09:36:47 +0900

    Time: 1 ms (99.33%), Space: 43 MB (26.38%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,8 +1,13 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++)
-            for (int j = 0; j < nums.length; j++)
-                if (i != j && nums[i] + nums[j] == target) return new int[]{i, j};
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) return new int[]{i, map.get(target - nums[i])};
+            map.put(nums[i], i);
+        }
         return null;
     }
 }
