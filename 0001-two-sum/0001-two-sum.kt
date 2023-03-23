236ca32703edb4150d55ab55fc69247d5375c162
JaeWon Choi <codesver@gmail.com>
2023-03-23 09:35:05 +0900

    Time: 216 ms (72.22%), Space: 38.5 MB (24.94%) - LeetHub

--- 0001-two-sum/0001-two-sum.kt
+++ 0001-two-sum/0001-two-sum.kt
@@ -1,10 +1,10 @@
 class Solution {
     fun twoSum(nums: IntArray, target: Int): IntArray {
-        val map = nums.indices.associateBy { nums[it] }
-        map.forEach {
-            if (map.containsKey(target - it.key) && map[target - it.key] != it.value)
-                return intArrayOf(it.value, map[target - it.key]!!)
+        val map = mutableMapOf<Int, Int>()
+        nums.indices.forEach {
+            if (map.containsKey(target - nums[it])) return intArrayOf(it, map[target - nums[it]]!!)
+            map[nums[it]] = it
         }
-        return nums.mapIndexed { index, i -> Pair(index, i) }.filter { it.second == target / 2 }.map { it.first }.toIntArray()
+        return intArrayOf()
     }
 }
