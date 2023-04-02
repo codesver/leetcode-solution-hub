ce4fef7021227ee92d779df1885db7ae8fce94c4
JaeWon Choi <codesver@gmail.com>
2023-04-02 19:58:52 +0900

    Time: 116 ms (92.60%), Space: 32.5 MB (97.53%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun climbStairs(n: Int): Int {
+        val dp = IntArray(n + 2)
+        dp[1] = 1
+        dp[2] = 2
+        for (i in 3..n) dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[n]
+    }
+}
