f77f84c8e0a05c5963ff7c06f81e0225abea8cd8
JaeWon Choi <codesver@gmail.com>
2023-04-10 10:36:06 +0900

    Time: 124 ms (98.59%), Space: 33.1 MB (99.29%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.kt
+++ 0020-valid-parentheses/0020-valid-parentheses.kt
@@ -1,9 +1,7 @@
-import java.util.*
+import java.util.Stack
 
 class Solution {
-    fun isValid(s: String): Boolean = object : Stack<Char>() {
-        init {
-            s.forEach { if (isNotEmpty() && it - peek() in 1..2) pop() else push(it) }
-        }
+    fun isValid(s: String): Boolean = Stack<Char>().apply {
+        s.forEach { if (isNotEmpty() && it - peek() in 1..2) pop() else push(it) }
     }.isEmpty()
 }
