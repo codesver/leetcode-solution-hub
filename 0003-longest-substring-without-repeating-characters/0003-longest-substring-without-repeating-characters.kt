9b6f1d1622f29a8a4029b4cd2ac91719e6396f82
JaeWon Choi <codesver@gmail.com>
2023-04-13 09:53:08 +0900

    Time: 195 ms (88.78%), Space: 36.7 MB (53.22%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.kt
@@ -0,0 +1,25 @@
+import kotlin.math.max
+
+class Solution {
+    fun lengthOfLongestSubstring(s: String): Int {
+        var start = 0
+        val map = HashMap<Char, Int>()
+        return s.toCharArray().mapIndexed { index, ch ->
+            start = max(start, (map[ch] ?: -1) + 1)
+            map[ch] = index
+            return@mapIndexed index - start + 1
+        }.maxOrNull() ?: 0
+    }
+}
+
+// Written because LeetCode Kotlin version is TOO LOW to use this function
+fun <T : Comparable<T>> Iterable<T>.maxOrNull(): T? {
+    val iterator = iterator()
+    if (!iterator.hasNext()) return null
+    var max = iterator.next()
+    while (iterator.hasNext()) {
+        val e = iterator.next()
+        if (max < e) max = e
+    }
+    return max
+}
