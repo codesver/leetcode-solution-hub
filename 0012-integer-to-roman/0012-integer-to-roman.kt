3084440f3d704b2473e2832cd3b2584f116173a7
JaeWon Choi <codesver@gmail.com>
2023-06-20 13:40:52 +0900

    Time: 202 ms (82.35%), Space: 37.4 MB (79.41%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.kt
@@ -0,0 +1,14 @@
+class Solution {
+    fun intToRoman(num: Int) = buildString {
+        var value = num
+        Roman.values().forEach { roman ->
+            append(roman.toString().repeat(value / roman.value))
+            value %= roman.value
+        }
+    }
+}
+
+enum class Roman(val value: Int) {
+    M(1000), CM(900), D(500), CD(400), C(100), XC(90),
+    L(50), XL(40), X(10), IX(9), V(5), IV(4), I(1)
+}
