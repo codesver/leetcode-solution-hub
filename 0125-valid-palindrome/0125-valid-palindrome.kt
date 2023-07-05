9b0966c928e4ffdaf239ee40a50a723a88b2b05c
JaeWon Choi <codesver@gmail.com>
2023-07-05 13:57:59 +0900

    Time: 204 ms (56.10%), Space: 37.2 MB (80.84%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.kt
+++ 0125-valid-palindrome/0125-valid-palindrome.kt
@@ -2,13 +2,13 @@ class Solution {
     fun isPalindrome(s: String): Boolean {
         var (front, back) = 0 to s.lastIndex
         while (front < back) {
-            if (s[front].isLetterOrDigit() && s[back].isLetterOrDigit()) {
-                if (s[front].equals(s[back], ignoreCase = true)) {
-                    front++
-                    back--
-                } else return false
-            } else if (!s[front].isLetterOrDigit()) front++
+            if (!s[front].isLetterOrDigit()) front++
             else if (!s[back].isLetterOrDigit()) back--
+            else if (!s[front].equals(s[back], ignoreCase = true)) return false
+            else {
+                front++
+                back--
+            }
         }
         return true
     }
