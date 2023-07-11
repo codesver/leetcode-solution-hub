04148c5c3e371314b71f225457d1e32b06463604
JaeWon Choi <codesver@gmail.com>
2023-07-11 13:47:01 +0900

    Time: 192 ms (14.81%), Space: 37.6 MB (12.35%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.kt
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.kt
@@ -1,7 +1,7 @@
 import kotlin.math.pow
 
 class Solution {
-    fun titleToNumber(columnTitle: String) = columnTitle.reversed().mapIndexed { i, c ->
-        (26.0).pow(i).toInt().times(c.toInt() - 64)
+    fun titleToNumber(title: String) = title.indices.map { i ->
+        (26.0).pow(i).toInt().times(title[title.length - i - 1].toInt() - 64)
     }.sum()
 }
